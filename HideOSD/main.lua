38e8f7cc50f5323e38af5619d84ee523146194de
thatnerd527 <muhammadfadhlan4@gmail.com>
2023-03-11 12:33:09 +0000

    dsadaas  asd cs

--- HideOSD/main.lua
+++ HideOSD/main.lua
@@ -1,8 +1,10 @@
 script.Parent.Equipped:Connect(function()
+    print("eb t")
 	game.Players.LocalPlayer.PlayerGui["Ragdoll & Get Up"].Enabled = true
 end)
 
 script.Parent.Unequipped:Connect(function ()
+  print("eb f")
   game.Players.LocalPlayer.PlayerGui["Ragdoll & Get Up"].Enabled = false
 end)
 
